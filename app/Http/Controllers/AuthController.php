e0ab7f805016fc82bcd4843d86c722b841db720c
justaclover <jane.thecat2308@gmail.com>
2025-04-08 10:39:56 +0300

    Обновление авторизации по кейклоку

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,12 +44,13 @@ class AuthController extends Controller
     public function keycloakCallback()
     {
         $requestUser = Socialite::driver('keycloak')->stateless()->user();
-        $token = $requestUser->token;
-
-        $publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4okswT9tjkxa7VLRu/jZ5lLF7Qf9HdeJtIN3gSqaEkNaDynq/ZRmuS+UUhVZTHRxrJBFy30EHV7q6g7A2vlaY5MJNf9+QYeaxLmajllLvoanY0/a6ThWNjaLln3lwUd1JC8NElEWcS6LEwCrm9HdLiNhs19wDro7V2pzEcPCg4h2Lw9jnjf9xIFEqS4LbkT4O+InHLQ/Yeq3JNJ1ObSWmShEpdvqb/6eqrnxr3Rbxz7O2SplMUgOyidrNIesEg5TBbPeROUzvCSbWLmAVB0yOk8s+kq1Zs2rzS4CpsTgpFtXwVRmXkMCLW34JDUE3F9U0A1xBetEcmQRurFOqZNiJwIDAQAB";
-        $publicKey = "-----BEGIN PUBLIC KEY-----\n" . chunk_split($publicKey, 64) . "-----END PUBLIC KEY-----";
-        $decoded = JWT::decode($token, new Key($publicKey, 'RS256'));
-        $groups = $decoded->groups;
+        //dd(json_encode($requestUser, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+//        $token = $requestUser->token;
+//
+//        $publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4okswT9tjkxa7VLRu/jZ5lLF7Qf9HdeJtIN3gSqaEkNaDynq/ZRmuS+UUhVZTHRxrJBFy30EHV7q6g7A2vlaY5MJNf9+QYeaxLmajllLvoanY0/a6ThWNjaLln3lwUd1JC8NElEWcS6LEwCrm9HdLiNhs19wDro7V2pzEcPCg4h2Lw9jnjf9xIFEqS4LbkT4O+InHLQ/Yeq3JNJ1ObSWmShEpdvqb/6eqrnxr3Rbxz7O2SplMUgOyidrNIesEg5TBbPeROUzvCSbWLmAVB0yOk8s+kq1Zs2rzS4CpsTgpFtXwVRmXkMCLW34JDUE3F9U0A1xBetEcmQRurFOqZNiJwIDAQAB";
+//        $publicKey = "-----BEGIN PUBLIC KEY-----\n" . chunk_split($publicKey, 64) . "-----END PUBLIC KEY-----";
+//        $decoded = JWT::decode($token, new Key($publicKey, 'RS256'));
+        $groups = $requestUser->groups;
 
         if (in_array("Developer", $groups) || in_array("IT", $groups)) {
             $user = User::updateOrCreate(['keycloak_id' => $requestUser->id], [
@@ -76,12 +77,12 @@ class AuthController extends Controller
     public function logout()
     {
         if (Auth::check()){
-            $token = Auth::user()->getRememberToken();
+            //$token = Auth::user()->getRememberToken();
             $keycloakLogoutUrl = config('services.keycloak.base_url')
                 . '/realms/' . config('services.keycloak.realms')
                 . '/protocol/openid-connect/logout';
             $params = [
-                'id_token_hint' => $token,
+                'client_id' => config('services.keycloak.client_id'),
                 'post_logout_redirect_uri' => url('/login'), // URL для перенаправления после логаута
             ];
 
