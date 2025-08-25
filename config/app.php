35008788312310182cb34551c338d33fa5ac30fa
Вячеслав Копаев <v.s.kopaev@edu.mtuci.ru>
2025-08-25 12:49:12 +0300

    feature: send heartbeat

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'heartbeat_link' => env('HEARTBEAT_TOKEN'),
 
     /*
     |--------------------------------------------------------------------------
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,18 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::call(function () {
+    $url = config('app.heartbeat_link');
+    if ($url) {
+        try {
+            $response = Http::get($url);
+            $this->info('Heartbeat sent to ' . $url . ': ' . $response->status());
+        } catch (\Exception $e) {
+            $this->error('Failed to send heartbeat to ' . $url . ': ' . $e->getMessage());
+        }
+    }
+})->everyTwoMinutes();
